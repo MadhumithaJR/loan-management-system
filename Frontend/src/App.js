36fa489e57bb2c90c37415b56143ef109fe37c82
Keerthana-sk11 <keethsk2001@gmail.com>
2023-09-26 14:42:13 +0530

    Home page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -19,34 +19,43 @@ import Employee from './components/Employee';
 const App = () => {
   return (
     <div className="App">
-    <header>
-      <h1>Loan Management App</h1>
+      <header>
+        <h1>Loan Management App</h1>
       </header>
-      <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
+      <section>
+        <div style={{
+          backgroundImage: "url(https://res.cloudinary.com/practicaldev/image/fetch/s--ddOXg3rr--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ko3w8f3icy7va6mmhx75.jpg)", backgroundSize: 'cover', backgroundRepeat: 'no-repeat', minHeight: '86vh', minWidth: '90vw'
+          //backgroundImage: "url(https://media.licdn.com/dms/image/D5612AQFAMCWk7Dkvvw/article-cover_image-shrink_600_2000/0/1672904516095?e=2147483647&v=beta&t=XXctsPTRnJ5CrCa9YGPHMpkQH5hjYpaXmQf97cMZLeM)", backgroundSize: 'cover', backgroundRepeat: 'no-repeat', minHeight: '86vh', minWidth: '90vw'
+          
+        }}>
+          <BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
 
-        <Route exact path="/admin-login" element={<AdminLogin/>}/>
-        <Route exact path="/login" element={<EmployeeLogin/>}/>
+              <Route exact path="/admin-login" element={<AdminLogin />} />
+              <Route exact path="/login" element={<EmployeeLogin />} />
 
-        <Route path='/addEmployee/:eid' element={<CreateUpdateEmployee/>}/>
-        <Route exact path="/manage-employee" element={<EmployeeView/>}/>
+              <Route path='/addEmployee/:eid' element={<CreateUpdateEmployee />} />
+              <Route exact path="/manage-employee" element={<EmployeeView />} />
 
-        <Route path='/addLoan/:lid' element={<CreateUpdateLoan/>}/>
-        <Route exact path="/manage-loan" element={<LoanView/>}/>
+              <Route path='/addLoan/:lid' element={<CreateUpdateLoan />} />
+              <Route exact path="/manage-loan" element={<LoanView />} />
 
-        <Route path='/addItem/:Iid' element={<CreateUpdateItem/>}/>
-        <Route exact path="/manage-item" element={<ItemView/>}/>
+              <Route path='/addItem/:Iid' element={<CreateUpdateItem />} />
+              <Route exact path="/manage-item" element={<ItemView />} />
 
-        <Route exact path="/register" element={<Register/>}/>
-        <Route exact path="/admin" element={<Admin/>}/>
-        <Route exact path="/employee" element={<Employee/>}/>
-        
-        <Route path="/user/:id/viewLoan" element={<ViewLoans/>}/>
-        <Route path="/user/:id/viewItems" element={<ViewItems/>}/>
-      </Routes>
-      </BrowserRouter>
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/admin" element={<Admin />} />
+              <Route exact path="/employee" element={<Employee />} />
+
+              <Route path="/user/:id/viewLoan" element={<ViewLoans />} />
+              <Route path="/user/:id/viewItems" element={<ViewItems />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </section>
     </div>
+
   );
 }
 
