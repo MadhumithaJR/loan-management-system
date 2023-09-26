637f4958c68b0b54bfa4cf397b3b2b580b32f278
DhyeyThakkar27 <dhyeythakkar2701@gmail.com>
2023-09-26 15:13:11 +0530

    animation

--- Frontend/src/components/AboutUs.js
+++ Frontend/src/components/AboutUs.js
@@ -1,20 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init({duration: 1200,once:true});
 const AboutUs = () => {
     return ( 
         <div>
             <div className="container-fluid">
-                <div className="row p-5 text-start w-75">
+                <div className="row p-5 text-start w-75" >
+                    <div data-aos = "fade-right">
                     <h5>About Us</h5>
                     <p>
                     Welcome to Loan management, your trusted partner in financial management and loan 
                     services.
                     <br />
-                    <br />
                     At Loan management, we understand the importance of financial stability and the 
                     role it plays in achieving your dreams and aspirations. That's why we're dedicated 
                     to providing you with a seamless and secure platform to manage your loans and make 
                     informed financial decisions.
-                    </p>            
+                    </p>   
+                    </div>
+                    <div data-aos = "fade-right">
                     <h5>Our Mission</h5>
                     <p>
                     Our mission is to empower individuals and businesses with the tools and knowledge they
@@ -22,7 +28,8 @@ const AboutUs = () => {
                     that everyone deserves access to fair and transparent loan solutions, and we're committed 
                     to making that a reality.
                     </p>
-
+                    </div>
+                    <div data-aos = "fade-right">
                     <h5>
                         Why choose us?
                     </h5>    
@@ -32,14 +39,23 @@ const AboutUs = () => {
                         <li class="list-group-item">Transparency</li>
                         <li class="list-group-item">Security</li>
                         <li class="list-group-item">Personalized Solutions</li>
-                    </ul>
-
+                    </ul> 
+                    </div>
+                    <div data-aos = "fade-right">
                     <h5>Join Us Today</h5>
-
+                    <p>
                     Whether you're an individual looking for a personal loan or a business 
                     owner seeking financial solutions, We are here to support you on your financial 
                     journey. Join our growing community of satisfied customers and experience the 
                     difference that our loan management app can make in your financial life.
+                    </p>
+                    </div>
+                             
+                    
+
+                    
+
+                    
                 </div>
 
             </div>
